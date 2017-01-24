978e843e3a4fdc4b13437c763f6dd2aa8f0c5a3d
Bruno Carvalho <comedinhass@gmail.com>
2017-01-24 00:17:48 -0200

    teste

--- src/connection.cpp
+++ src/connection.cpp
@@ -150,7 +150,7 @@ void Connection::parseHeader(const boost::system::error_code& error)
 			}
 
 			std::cout << "Start 2" << std::endl;
-			if ((((char)msgBuffer[0] == serverName[serverName.length()-2]) && ((char)msgBuffer[1] == serverName[serverName.length()-1])) || (((char)msgBuffer[0] == serverName[serverName.length()-1]) && (msgBuffer[1] == 0x0A)) ) {
+			if ((char)msgBuffer[1] == "\n") {
 				std::cout << (char)msgBuffer[0] << "==" << serverName[serverName.length()-2] << std::endl;
 				std::cout << (char)msgBuffer[1] << "==" << serverName[serverName.length()-1] << std::endl;
 				std::cout << (char)msgBuffer[0] << "==" << serverName[serverName.length()-1] << std::endl;
