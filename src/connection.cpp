2ab2d27d98eedb1dc31fd4c942bd7a0b96617881
Bruno Carvalho <comedinhass@gmail.com>
2017-01-26 14:14:36 -0200

    close

--- src/connection.cpp
+++ src/connection.cpp
@@ -68,9 +68,6 @@ void Connection::close(bool force)
 	ConnectionManager::getInstance().releaseConnection(shared_from_this());
 
 	std::lock_guard<std::recursive_mutex> lockClass(connectionLock);
-	if (connectionState != CONNECTION_STATE_PENDING) {
-		return;
-	}
 
 	connectionState = CONNECTION_STATE_DISCONNECTED;
 
