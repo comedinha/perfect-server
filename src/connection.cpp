13081d90c8ba7a36546c5b38f3353771dff03887
Bruno Carvalho <comedinhass@gmail.com>
2017-01-26 15:19:12 -0200

    test

--- src/connection.cpp
+++ src/connection.cpp
@@ -136,7 +136,8 @@ void Connection::parseHeader(const boost::system::error_code& error)
 {
 	if (!receivedLastChar) {
 		if (connectionState == CONNECTION_STATE_CONNECTING_STAGE2) {
-			msg.skipBytes(7);
+			std::string serverName = g_config.getString(ConfigManager::SERVER_NAME);
+			msg.skipBytes(serverName.length() * 4);
 			return;
 
 			uint8_t* msgBuffer = msg.getBuffer();
