d185580f8c184bb859d57210d218b6df9a51349a
Bruno Carvalho <comedinhass@gmail.com>
2017-01-27 09:39:56 -0200

    protection

--- src/connection.cpp
+++ src/connection.cpp
@@ -149,19 +149,24 @@ void Connection::parseHeader(const boost::system::error_code& error)
 				receivedLastChar = true;
 			}
 		} else {
-			readTimer.expires_from_now(boost::posix_time::seconds(Connection::read_timeout));
-			readTimer.async_wait(std::bind(&Connection::handleTimeout, std::weak_ptr<Connection>(shared_from_this()), std::placeholders::_1));
+			try {
+				readTimer.expires_from_now(boost::posix_time::seconds(Connection::read_timeout));
+				readTimer.async_wait(std::bind(&Connection::handleTimeout, std::weak_ptr<Connection>(shared_from_this()), std::placeholders::_1));
 
-			if ((char)msgBuffer[0] == lastChar[0]) {
-				receivedLastChar = true;
-				boost::asio::async_read(socket,
-								boost::asio::buffer(msg.getBuffer(), NetworkMessage::HEADER_LENGTH),
-								std::bind(&Connection::parseHeader, shared_from_this(), std::placeholders::_1));
-			} else {
-				// Read size of the server name packet
-				boost::asio::async_read(socket,
-										boost::asio::buffer(msg.getBuffer(), 1),
-										std::bind(&Connection::parseHeader, shared_from_this(), std::placeholders::_1));
+				if ((char)msgBuffer[0] == lastChar[0]) {
+					receivedLastChar = true;
+					boost::asio::async_read(socket,
+									boost::asio::buffer(msg.getBuffer(), NetworkMessage::HEADER_LENGTH),
+									std::bind(&Connection::parseHeader, shared_from_this(), std::placeholders::_1));
+				} else {
+					// Read size of the server name packet
+					boost::asio::async_read(socket,
+											boost::asio::buffer(msg.getBuffer(), 1),
+											std::bind(&Connection::parseHeader, shared_from_this(), std::placeholders::_1));
+				}
+			} catch (boost::system::system_error& e) {
+				std::cout << "[Network error - Connection::parseHeader] " << e.what() << std::endl;
+				close(FORCE_CLOSE);
 			}
 			return;
 		}
