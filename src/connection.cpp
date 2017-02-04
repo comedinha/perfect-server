85d7db48f6050d622e316fc0a302b3d3c6f6413a
Bruno Carvalho <comedinhass@gmail.com>
2017-02-04 21:08:46 -0200

    oops

--- src/connection.cpp
+++ src/connection.cpp
@@ -153,9 +153,17 @@ void Connection::parseHeader(const boost::system::error_code& error)
 			std::string serverName = g_config.getString(ConfigManager::SERVER_NAME) + "\n";
 			receivedLastChar = true;
 
-			boost::asio::async_read(socket,
+			try {
+				readTimer.expires_from_now(boost::posix_time::seconds(Connection::read_timeout));
+				readTimer.async_wait(std::bind(&Connection::handleTimeout, std::weak_ptr<Connection>(shared_from_this()), std::placeholders::_1));
+
+				boost::asio::async_read(socket,
 									boost::asio::buffer(msg.getBuffer(), serverName.length()),
 									std::bind(&Connection::parseHeader, shared_from_this(), std::placeholders::_1));
+				} catch (boost::system::system_error& e) {
+					std::cout << "[Network error - Connection::accept] " << e.what() << std::endl;
+					close(FORCE_CLOSE);
+				}
 			return;
 		}
 	}
